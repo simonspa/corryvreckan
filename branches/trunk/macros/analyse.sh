9b7e6cec2c3d74625efc321fe240be481f04698f
CLIC Telescope <clic.tel@cern.ch>
2015-10-20 18:30:18 +0200

    updated analyse.sh
    Former-commit-id: 922c9e38050649464aad91b01f15de27e5532e53

--- branches/trunk/macros/analyse.sh
+++ branches/trunk/macros/analyse.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 TBCODE=..
-RUN=25
-
-DATADIR=${TBCODE}/example/Run${RUN}
-CONDFILE=${TBCODE}/cond/cond.dat
+RUN=$1
+ 
+DATADIR=/data/tpx3test/data/Run${RUN}
+CONDFILE=alignmentSaved.dat
+#CONDFILE=../cond/alignmentRun880.dat
 HISTOFILE=${TBCODE}/histos/histogramsRun${RUN}.root
-NEVENTS=100
-EVENTTIME=0
+NEVENTS=-1
+EVENTTIME=0.0002
 
-${TBCODE}/bin/tbAnalysis -d ${DATADIR} -c ${CONDFILE} -n ${NEVENTS} -h ${HISTOFILE} -t ${EVENTTIME} -g
+${TBCODE}/bin/tbAnalysis -d ${DATADIR} -c ${CONDFILE} -n ${NEVENTS} -h ${HISTOFILE} -t ${EVENTTIME} 
+ 
