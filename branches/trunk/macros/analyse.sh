33409ad6ebce44b178445dd97aa0f2666c4ee35c
CLIC Telescope <clic.tel@cern.ch>
2015-10-21 14:15:24 +0200

    add start time flag to options file
    Former-commit-id: 58ef42bc0e9c57d134f20b47d085d10139e3f7fe

--- branches/trunk/macros/analyse.sh
+++ branches/trunk/macros/analyse.sh
@@ -8,7 +8,8 @@ CONDFILE=alignmentSaved.dat
 #CONDFILE=../cond/alignmentRun880.dat
 HISTOFILE=${TBCODE}/histos/histogramsRun${RUN}.root
 NEVENTS=-1
-EVENTTIME=0.0002
+EVENTTIME=0.00005
+STARTTIME=18.0
 
-${TBCODE}/bin/tbAnalysis -d ${DATADIR} -c ${CONDFILE} -n ${NEVENTS} -h ${HISTOFILE} -t ${EVENTTIME} 
+${TBCODE}/bin/tbAnalysis -d ${DATADIR} -c ${CONDFILE} -n ${NEVENTS} -h ${HISTOFILE} -t ${EVENTTIME} -o ${STARTTIME}
  
