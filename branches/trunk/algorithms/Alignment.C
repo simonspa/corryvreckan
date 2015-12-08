f277e3bc72c4acfe6f95ac8d21cf2fa4fce95ed3
Daniel Hynds <daniel.hynds@cern.ch>
2015-12-08 13:41:51 +0100

    added iteration into alignment
    Former-commit-id: 237eb49d6ede7c30170c7bdf523885de88d31e87

--- branches/trunk/algorithms/Alignment.C
+++ branches/trunk/algorithms/Alignment.C
@@ -5,7 +5,8 @@
 Alignment::Alignment(bool debugging)
 : Algorithm("Alignment"){
   debug = debugging;
-  m_numberOfTracksForAlignment = 8000;
+  m_numberOfTracksForAlignment = 100000;
+  nIterations = 5;
 }
 
 // Global container declarations
@@ -177,17 +178,21 @@ void Alignment::finalise(){
     residualFitter->SetParameter(4,(detectorID+"_rotationY").c_str(),parameters->detector[detectorID]->rotationY(),0.001,-6.30,6.30);
     residualFitter->SetParameter(5,(detectorID+"_rotationZ").c_str(),parameters->detector[detectorID]->rotationZ(),0.001,-6.30,6.30);
     
-    // Fit this plane (minimising global track chi2)
-    residualFitter->ExecuteCommand("MIGRAD",arglist,2);
+    for(int iteration=0;iteration<nIterations;iteration++){
+    
+      // Fit this plane (minimising global track chi2)
+    	residualFitter->ExecuteCommand("MIGRAD",arglist,2);
    
-    // Set the alignment parameters of this plane to be the optimised values from the alignment
-    parameters->detector[detectorID]->displacementX(residualFitter->GetParameter(0));
-    parameters->detector[detectorID]->displacementY(residualFitter->GetParameter(1));
-    parameters->detector[detectorID]->displacementZ(residualFitter->GetParameter(2));
-    parameters->detector[detectorID]->rotationX(residualFitter->GetParameter(3));
-    parameters->detector[detectorID]->rotationY(residualFitter->GetParameter(4));
-    parameters->detector[detectorID]->rotationZ(residualFitter->GetParameter(5));
+      // Set the alignment parameters of this plane to be the optimised values from the alignment
+      parameters->detector[detectorID]->displacementX(residualFitter->GetParameter(0));
+      parameters->detector[detectorID]->displacementY(residualFitter->GetParameter(1));
+      parameters->detector[detectorID]->displacementZ(residualFitter->GetParameter(2));
+      parameters->detector[detectorID]->rotationX(residualFitter->GetParameter(3));
+      parameters->detector[detectorID]->rotationY(residualFitter->GetParameter(4));
+      parameters->detector[detectorID]->rotationZ(residualFitter->GetParameter(5));
 
+    }
+    
     // Write the output alignment file
     parameters->writeConditions();
 
@@ -199,45 +204,50 @@ void Alignment::finalise(){
   // each minimisation step.
   
   int det = 0;
-  for(int ndet = 0; ndet<parameters->nDetectors; ndet++){
-    // Check if they are a Timepix3
-    string detectorID = parameters->detectors[ndet];
-    if(parameters->detector[detectorID]->type() != "Timepix3") continue;
-    // Do not align the reference plane
-    if(detectorID == parameters->reference) continue;
-    if(parameters->excludedFromTracking.count(detectorID) != 0) continue;
-    // Say that this is the detector we align
-    detectorToAlign = detectorID;
-    detNum=det;
-    // Add the parameters to the fitter (z displacement not allowed to move!)
-    residualFitter->SetParameter(det*6+0,(detectorID+"_displacementX").c_str(),parameters->detector[detectorID]->displacementX(),0.01,-50,50);
-    residualFitter->SetParameter(det*6+1,(detectorID+"_displacementY").c_str(),parameters->detector[detectorID]->displacementY(),0.01,-50,50);
-    residualFitter->SetParameter(det*6+2,(detectorID+"_displacementZ").c_str(),parameters->detector[detectorID]->displacementZ(),0,-10,500);
-    residualFitter->SetParameter(det*6+3,(detectorID+"_rotationX").c_str(),parameters->detector[detectorID]->rotationX(),0.001,-6.30,6.30);
-    residualFitter->SetParameter(det*6+4,(detectorID+"_rotationY").c_str(),parameters->detector[detectorID]->rotationY(),0.001,-6.30,6.30);
-    residualFitter->SetParameter(det*6+5,(detectorID+"_rotationZ").c_str(),parameters->detector[detectorID]->rotationZ(),0.001,-6.30,6.30);
-    
-    // Fit this plane (minimising global track chi2)
-		residualFitter->ExecuteCommand("MIGRAD",arglist,2);
-    
-    // Now that this device is fitted, set parameter errors to 0 so that they are not fitted again
-    residualFitter->SetParameter(det*6+0,(detectorID+"_displacementX").c_str(),residualFitter->GetParameter(det*6+0),0,-50,50);
-    residualFitter->SetParameter(det*6+1,(detectorID+"_displacementY").c_str(),residualFitter->GetParameter(det*6+1),0,-50,50);
-    residualFitter->SetParameter(det*6+2,(detectorID+"_displacementZ").c_str(),residualFitter->GetParameter(det*6+2),0,-10,500);
-    residualFitter->SetParameter(det*6+3,(detectorID+"_rotationX").c_str(),residualFitter->GetParameter(det*6+3),0,-6.30,6.30);
-    residualFitter->SetParameter(det*6+4,(detectorID+"_rotationY").c_str(),residualFitter->GetParameter(det*6+4),0,-6.30,6.30);
-    residualFitter->SetParameter(det*6+5,(detectorID+"_rotationZ").c_str(),residualFitter->GetParameter(det*6+5),0,-6.30,6.30);
-    // Set the alignment parameters of this plane to be the optimised values from the alignment
-    parameters->detector[detectorID]->displacementX(residualFitter->GetParameter(det*6+0));
-    parameters->detector[detectorID]->displacementY(residualFitter->GetParameter(det*6+1));
-    parameters->detector[detectorID]->displacementZ(residualFitter->GetParameter(det*6+2));
-    parameters->detector[detectorID]->rotationX(residualFitter->GetParameter(det*6+3));
-    parameters->detector[detectorID]->rotationY(residualFitter->GetParameter(det*6+4));
-    parameters->detector[detectorID]->rotationZ(residualFitter->GetParameter(det*6+5));
-    parameters->detector[detectorID]->update();
-    det++;
-  }
+  for(int iteration=0;iteration<nIterations;iteration++){
+
+    det = 0;
+    for(int ndet = 0; ndet<parameters->nDetectors; ndet++){
+      // Check if they are a Timepix3
+      string detectorID = parameters->detectors[ndet];
+      if(parameters->detector[detectorID]->type() != "Timepix3") continue;
+      // Do not align the reference plane
+      if(detectorID == parameters->reference) continue;
+      if(parameters->excludedFromTracking.count(detectorID) != 0) continue;
+      // Say that this is the detector we align
+      detectorToAlign = detectorID;
+      detNum=det;
+      // Add the parameters to the fitter (z displacement not allowed to move!)
+      residualFitter->SetParameter(det*6+0,(detectorID+"_displacementX").c_str(),parameters->detector[detectorID]->displacementX(),0.01,-50,50);
+      residualFitter->SetParameter(det*6+1,(detectorID+"_displacementY").c_str(),parameters->detector[detectorID]->displacementY(),0.01,-50,50);
+      residualFitter->SetParameter(det*6+2,(detectorID+"_displacementZ").c_str(),parameters->detector[detectorID]->displacementZ(),0,-10,500);
+      residualFitter->SetParameter(det*6+3,(detectorID+"_rotationX").c_str(),parameters->detector[detectorID]->rotationX(),0.001,-6.30,6.30);
+      residualFitter->SetParameter(det*6+4,(detectorID+"_rotationY").c_str(),parameters->detector[detectorID]->rotationY(),0.001,-6.30,6.30);
+      residualFitter->SetParameter(det*6+5,(detectorID+"_rotationZ").c_str(),parameters->detector[detectorID]->rotationZ(),0.001,-6.30,6.30);
+      
+      // Fit this plane (minimising global track chi2)
+      residualFitter->ExecuteCommand("MIGRAD",arglist,2);
+      
+      // Now that this device is fitted, set parameter errors to 0 so that they are not fitted again
+      residualFitter->SetParameter(det*6+0,(detectorID+"_displacementX").c_str(),residualFitter->GetParameter(det*6+0),0,-50,50);
+      residualFitter->SetParameter(det*6+1,(detectorID+"_displacementY").c_str(),residualFitter->GetParameter(det*6+1),0,-50,50);
+      residualFitter->SetParameter(det*6+2,(detectorID+"_displacementZ").c_str(),residualFitter->GetParameter(det*6+2),0,-10,500);
+      residualFitter->SetParameter(det*6+3,(detectorID+"_rotationX").c_str(),residualFitter->GetParameter(det*6+3),0,-6.30,6.30);
+      residualFitter->SetParameter(det*6+4,(detectorID+"_rotationY").c_str(),residualFitter->GetParameter(det*6+4),0,-6.30,6.30);
+      residualFitter->SetParameter(det*6+5,(detectorID+"_rotationZ").c_str(),residualFitter->GetParameter(det*6+5),0,-6.30,6.30);
+      
+      // Set the alignment parameters of this plane to be the optimised values from the alignment
+      parameters->detector[detectorID]->displacementX(residualFitter->GetParameter(det*6+0));
+      parameters->detector[detectorID]->displacementY(residualFitter->GetParameter(det*6+1));
+      parameters->detector[detectorID]->displacementZ(residualFitter->GetParameter(det*6+2));
+      parameters->detector[detectorID]->rotationX(residualFitter->GetParameter(det*6+3));
+      parameters->detector[detectorID]->rotationY(residualFitter->GetParameter(det*6+4));
+      parameters->detector[detectorID]->rotationZ(residualFitter->GetParameter(det*6+5));
+      parameters->detector[detectorID]->update();
+      det++;
+    }
   
+  }
   det = 0;
 
   // Now list the new alignment parameters
