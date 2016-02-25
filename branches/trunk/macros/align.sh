5499705c943a81d9507d99c87aba4a63c7bbd8e6
Daniel Hynds <dhynds@pclcd28.cern.ch>
2016-02-25 12:50:05 +0100

    more generic align.sh with DB search for alignment
    Former-commit-id: 933accb075c2c96a89bc5ff6dbd3d405a5572442

--- branches/trunk/macros/align.sh
+++ branches/trunk/macros/align.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
+# Where the code is and which run to analyse
 TBCODE=..
-RUN=1100
+RUN=${1}
  
-DATADIR=${TBCODE}/example/Run${RUN}
-CONDFILE=${TBCODE}/cond/alignmentRun${RUN}.dat
-HISTOFILE=${TBCODE}/histos/histogramsRun${RUN}.root
+# Set up the arguments to be passed to the testbeam code
+CONDFILE=${TBCODE}/cond/Alignment${RUN}.dat
+HISTOFILE=${TBCODE}/histos/alignmentHistogramsRun${RUN}.root
+INPUTFILE=${TBCODE}/pixels/pixelsRun${RUN}.root
 NEVENTS=-1
-EVENTTIME=0.00005
-STARTTIME=0
 
-${TBCODE}/bin/tbAnalysis -d ${DATADIR} -c ${CONDFILE} -n ${NEVENTS} -h ${HISTOFILE} -t ${EVENTTIME} -o ${STARTTIME} -a 0
-mv alignmentOutput.dat ${CONDFILE}
+# If the alignment file for this run does not exist, get it from the DB
+if [ ! -f ${CONDFILE} ] 
+then
+  while read run align dut thl angle
+  do
+    if [ "$run" == "$RUN" ]
+    then
+      CONDFILE=${TBCODE}/cond/Alignment${align}.dat
+      echo New cond file is ${CONDFILE}
+    fi
+  done <<< "$(grep ${RUN} runListAngledHVCMOS.dat)"
+fi
 
-${TBCODE}/bin/tbAnalysis -d ${DATADIR} -c ${CONDFILE} -n ${NEVENTS} -h ${HISTOFILE} -t ${EVENTTIME} -o ${STARTTIME} -a 0
+# Launch the testbeam analysis and replace the original alignment file with the new one
+${TBCODE}/bin/tbAnalysis -c ${CONDFILE} -n ${NEVENTS} -h ${HISTOFILE} -t ${INPUTFILE} -a 0
 mv alignmentOutput.dat ${CONDFILE}
-
-${TBCODE}/bin/tbAnalysis -d ${DATADIR} -c ${CONDFILE} -n ${NEVENTS} -h ${HISTOFILE} -t ${EVENTTIME} -o ${STARTTIME} -a 1
-mv alignmentOutput.dat ${CONDFILE}
-
-${TBCODE}/bin/tbAnalysis -d ${DATADIR} -c ${CONDFILE} -n ${NEVENTS} -h ${HISTOFILE} -t ${EVENTTIME} -o ${STARTTIME} -a 1
-mv alignmentOutput.dat ${CONDFILE}
+ 
