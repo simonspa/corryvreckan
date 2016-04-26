39033b140e609f020ba59ca3aefe918fdb7ed2ef
Daniel Hynds <dhynds@lxplus023.cern.ch>
2016-04-26 13:13:55 +0200

    removed obsolete folder
    Former-commit-id: d021dca22ae1afa2c3a2e90bfd36745f56bad01b

--- obsolete/macros/merge.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ROOT=/home/dhynds/tbcode/monitoring
-RUN=$1
-
-hadd -f ${ROOT}/histograms${RUN}.root ${ROOT}/histograms${RUN}_a.root ${ROOT}/histograms${RUN}_b.root ${ROOT}/histograms${RUN}_c.root ${ROOT}/histograms${RUN}_d.root
