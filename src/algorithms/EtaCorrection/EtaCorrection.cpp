19ebc6f9bdcc675fc09a0d0bb914f3b4d566bce8
Simon Spannagel <simon.spannagel@cern.ch>
2018-03-09 10:39:17 +0100

    Update EtaCorrection: now works for all detectors. Put before tracking! Does not calculate distribution anymore

--- src/algorithms/EtaCorrection/EtaCorrection.cpp
+++ src/algorithms/EtaCorrection/EtaCorrection.cpp
@@ -5,112 +5,98 @@ using namespace std;
 
 EtaCorrection::EtaCorrection(Configuration config, std::vector<Detector*> detectors)
     : Algorithm(std::move(config), std::move(detectors)) {
-    m_DUT = m_config.get<std::string>("DUT");
     m_chi2ndofCut = m_config.get<double>("chi2ndofCut", 100.);
     m_etaFormulaX = m_config.get<std::string>("EtaFormulaX", "[0] + [1]*x + [2]*x^2 + [3]*x^3 + [4]*x^4 + [5]*x^5");
-    m_etaConstantsX = m_config.getArray<double>("EtaConstantsX", {});
     m_etaFormulaY = m_config.get<std::string>("EtaFormulaY", "[0] + [1]*x + [2]*x^2 + [3]*x^3 + [4]*x^4 + [5]*x^5");
-    m_etaConstantsY = m_config.getArray<double>("EtaConstantsY", {});
 }
 
 void EtaCorrection::initialise() {
 
-    // Initialise single histograms
-    m_detector = get_detector(m_DUT);
-    double pitchX = m_detector->pitchX();
-    double pitchY = m_detector->pitchY();
-    string name = "etaDistributionX";
-    m_etaDistributionX = new TH2F(name.c_str(), name.c_str(), 100., 0., pitchX, 100., 0., pitchY);
-    name = "etaDistributionY";
-    m_etaDistributionY = new TH2F(name.c_str(), name.c_str(), 100., 0., pitchX, 100., 0., pitchY);
-    name = "etaDistributionXprofile";
-    m_etaDistributionXprofile = new TProfile(name.c_str(), name.c_str(), 100., 0., pitchX, 0., pitchY);
-    name = "etaDistributionYprofile";
-    m_etaDistributionYprofile = new TProfile(name.c_str(), name.c_str(), 100., 0., pitchX, 0., pitchY);
-    name = "etaDistributionXcorrected";
-    m_etaDistributionXcorrected = new TH2F(name.c_str(), name.c_str(), 100., 0., pitchX, 100., 0., pitchY);
-    name = "etaDistributionYcorrected";
-    m_etaDistributionYcorrected = new TH2F(name.c_str(), name.c_str(), 100., 0., pitchX, 100., 0., pitchY);
+    // Initialise histograms
+    for(auto& detector : get_detectors()) {
+        double pitchX = detector->pitchX();
+        double pitchY = detector->pitchY();
 
-    // Initialise member variables
-    m_etaCorrectorX = new TF1("etaCorrectorX", m_etaFormulaX.c_str(), 0, m_detector->pitchX());
-    for(int x = 0; x < m_etaConstantsX.size(); x++)
-        m_etaCorrectorX->SetParameter(x, m_etaConstantsX[x]);
-    m_etaCorrectorY = new TF1("etaCorrectorY", m_etaFormulaY.c_str(), 0, m_detector->pitchY());
-    for(int y = 0; y < m_etaConstantsY.size(); y++)
-        m_etaCorrectorY->SetParameter(y, m_etaConstantsY[y]);
+        // Get info from configuration:
+        std::vector<double> m_etaConstantsX = m_config.getArray<double>("EtaConstantsX_" + detector->name(), {});
+        std::vector<double> m_etaConstantsY = m_config.getArray<double>("EtaConstantsY_" + detector->name(), {});
+        if(!m_etaConstantsX.empty() || !m_etaConstantsY.empty()) {
+            LOG(INFO) << "Found Eta correction factors for detector \"" << detector->name()
+                      << "\": " << (m_etaConstantsX.empty() ? "" : "X ") << (m_etaConstantsY.empty() ? "" : "Y ");
+        }
+
+        if(!m_etaConstantsX.empty()) {
+            m_correctX[detector->name()] = true;
+            m_etaCorrectorX[detector->name()] = new TF1("etaCorrectorX", m_etaFormulaX.c_str(), 0, detector->pitchX());
+            for(int x = 0; x < m_etaConstantsX.size(); x++) {
+                m_etaCorrectorX[detector->name()]->SetParameter(x, m_etaConstantsX[x]);
+            }
+        } else {
+            m_correctX[detector->name()] = false;
+        }
+
+        if(!m_etaConstantsY.empty()) {
+            m_correctY[detector->name()] = true;
+            m_etaCorrectorY[detector->name()] = new TF1("etaCorrectorY", m_etaFormulaY.c_str(), 0, detector->pitchY());
+            for(int y = 0; y < m_etaConstantsY.size(); y++)
+                m_etaCorrectorY[detector->name()]->SetParameter(y, m_etaConstantsY[y]);
+        } else {
+            m_correctY[detector->name()] = false;
+        }
+    }
     m_eventNumber = 0;
+}
+
+void EtaCorrection::applyEta(Cluster* cluster, Detector* detector) {
+    // Ignore single pixel clusters
+    if(cluster->size() == 1) {
+        return;
+    }
+
+    double newX = cluster->localX();
+    double newY = cluster->localY();
+
+    if(cluster->columnWidth() == 2) {
+        double inPixelX = detector->pitchX() * (cluster->column() - floor(cluster->column()));
+
+        // Apply the eta correction
+        if(m_correctX[detector->name()]) {
+            newX = floor(cluster->localX() / detector->pitchX()) * detector->pitchX() +
+                   m_etaCorrectorX[detector->name()]->Eval(inPixelX);
+        }
+    }
+
+    if(cluster->rowWidth() == 2) {
+        double inPixelY = detector->pitchY() * (cluster->row() - floor(cluster->row()));
+
+        // Apply the eta correction
+        if(m_correctY[detector->name()]) {
+            newY = floor(cluster->localY() / detector->pitchY()) * detector->pitchY() +
+                   m_etaCorrectorY[detector->name()]->Eval(inPixelY);
+        }
+    }
+
+    PositionVector3D<Cartesian3D<double>> positionLocal(newX, newY, 0);
+    PositionVector3D<Cartesian3D<double>> positionGlobal = detector->localToGlobal(positionLocal);
+    cluster->setClusterCentre(positionGlobal);
+    cluster->setClusterCentreLocal(positionLocal);
 }
 
 StatusCode EtaCorrection::run(Clipboard* clipboard) {
 
-    // Get the tracks from the clipboard
-    Tracks* tracks = (Tracks*)clipboard->get("tracks");
-    if(tracks == NULL) {
-        LOG(DEBUG) << "No tracks on the clipboard";
-        return Success;
-    }
-
-    // Loop over all tracks and look at the associated clusters to plot the eta distribution
-    for(auto& track : (*tracks)) {
-
-        // Cut on the chi2/ndof
-        if(track->chi2ndof() > m_chi2ndofCut) {
-            continue;
+    for(auto& detector : get_detectors()) {
+        // Get the clusters
+        Clusters* clusters = (Clusters*)clipboard->get(detector->name(), "clusters");
+        if(clusters == NULL) {
+            LOG(DEBUG) << "Detector " << detector->name() << " does not have any clusters on the clipboard";
+            return Success;
         }
 
-        // Get the in-pixel track intercept
-        PositionVector3D<Cartesian3D<double>> trackIntercept = m_detector->getIntercept(track);
-        PositionVector3D<Cartesian3D<double>> trackInterceptLocal = m_detector->globalToLocal(trackIntercept);
-        double pixelInterceptX = m_detector->inPixelX(trackInterceptLocal);
-        double pixelInterceptY = m_detector->inPixelY(trackInterceptLocal);
-        (pixelInterceptX > m_detector->pitchX() / 2. ? pixelInterceptX -= m_detector->pitchX() / 2.
-                                                     : pixelInterceptX += m_detector->pitchX() / 2.);
-        (pixelInterceptY > m_detector->pitchY() / 2. ? pixelInterceptY -= m_detector->pitchY() / 2.
-                                                     : pixelInterceptY += m_detector->pitchY() / 2.);
-
-        // Look at the associated clusters and plot the eta function
-        for(auto& dutCluster : track->associatedClusters()) {
-
-            // Only look at clusters from the DUT
-            if(dutCluster->detectorID() != m_DUT)
-                continue;
-
-            // Ignore single pixel clusters
-            if(dutCluster->size() == 1)
-                continue;
-
-            // Get the fraction along the pixel
-            double inPixelX = m_detector->pitchX() * (dutCluster->column() - floor(dutCluster->column()));
-            double inPixelY = m_detector->pitchY() * (dutCluster->row() - floor(dutCluster->row()));
-            if(dutCluster->columnWidth() == 2) {
-                m_etaDistributionX->Fill(inPixelX, pixelInterceptX);
-                m_etaDistributionXprofile->Fill(inPixelX, pixelInterceptX);
-
-                // Apply the eta correction
-                double newX = floor(dutCluster->localX() / m_detector->pitchX()) * m_detector->pitchX() +
-                              m_etaCorrectorX->Eval(inPixelX);
-
-                PositionVector3D<Cartesian3D<double>> positionLocal(newX, dutCluster->localY(), 0);
-                PositionVector3D<Cartesian3D<double>> positionGlobal = m_detector->localToGlobal(positionLocal);
-                dutCluster->setClusterCentre(positionGlobal);
-                dutCluster->setClusterCentreLocal(positionLocal);
-            }
-            if(dutCluster->rowWidth() == 2) {
-                m_etaDistributionY->Fill(inPixelY, pixelInterceptY);
-                m_etaDistributionYprofile->Fill(inPixelY, pixelInterceptY);
-            }
+        for(auto& cluster : (*clusters)) {
+            applyEta(cluster, detector);
         }
     }
-
-    // Increment event counter
-    m_eventNumber++;
 
     // Return value telling analysis to keep running
     return Success;
-}
-
-void EtaCorrection::finalise() {
-
-    LOG(DEBUG) << "Analysed " << m_eventNumber << " events";
 }
--- src/algorithms/EtaCorrection/README.md
+++ src/algorithms/EtaCorrection/README.md
@@ -1,22 +1,23 @@
 ## EtaCorrection
-**Maintainer**: Daniel Hynds (<daniel.hynds@cern.ch>)   
-**Status**: Under development
+**Maintainer**: Daniel Hynds (<daniel.hynds@cern.ch>), Simon Spannagel (<simon.spannagel@cern.ch>)  
+**Status**: Under development  
 
 #### Description
-This algorithm takes data from Timepix3 devices from the clipboard and plots the pixel hit positions to create the eta correction. Eta correction has still to be implemented in this algorithm (under development).
+This algorithm applies previously determined $`\eta`$-corrections to cluster positions of any detector. Corrections can be applied to any cluster read from the clipboard. The correction function as well as the parameters for each of the detectors can be given separately for X and Y via the configuration file.
+
+This algorithm does not calculate the $`\eta`$ distribution.
 
 #### Parameters
-No parameters are used from the configuration file.
+* `EtaFormulaX` / `EtaFormulaY`: The formula for the $`\eta`$ correction to be applied for the X an Y coordinate, respectively. It defaults to a polynomial of fifth ordern, i.e. `[0] + [1]*x + [2]*x^2 + [3]*x^3 + [4]*x^4 + [5]*x^5`.
+* `EtaConstantsX_<detector>` / `EtaConstantsY_<detector>`: Vector of correction factors, representing the parameters of the above correction function, in X and Y coordinates, respectively. Defaults to an empty vector, i.e. by default no correction is applied. The `<detector>` part of the variabe has to be replaced with the respective unique name of the detector given in the setup file.
 
 #### Plots produced
-* Histogram of event numbers
-
-For each detector the following plots are produced:
-* 2D histogram of pixel hit positions
+Currently, no plots are produced. The result of the $`\eta`$-correction is best observed in residual distributions of the respective detector.
 
 #### Usage
 ```toml
 [EtaCorrection]
-
+EtaFormulaX = [0] + [1]*x + [2]*x^2 + [3]*x^3 + [4]*x^4 + [5]*x^5
+EtaConstantsX_dut = 0.025 0.038  6.71 -323.58  5950.3 -34437.5
 ```
 Parameters to be used in multiple algorithms can also be defined globally at the top of the configuration file. This is highly encouraged for parameters such as `DUT` and `reference`.
