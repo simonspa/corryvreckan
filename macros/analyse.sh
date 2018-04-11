4abb30d08d7eadaed34101318aec6a8536a2c3fb
clictelescope machine <clic.user@cern.ch>
2018-04-11 09:58:07 +0200

    Update analyse script

--- macros/analyse.sh
+++ macros/analyse.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 CORRY=/home/telescope/corryvreckan
 RUN=${1}
-ALIGNMENT=27834
+ALIGNMENT=28300
 
 # Set up the arguments to be passed to Corryvreckan
-CONDFILE=${CORRY}/cond/Alignment${ALIGNMENT}.dat
+CONDFILE=${CORRY}/cond/Alignment${ALIGNMENT}_aligning.conf
 HISTOGRAMFILE=${CORRY}/macros/histograms.root
-INPUTDIR_TPX=/data/tbSeptember2017/data/Run${RUN}
-INPUTDIR_CPX=/data/tbSeptember2017/clicpix2/Run${RUN}
+INPUTDIR_TPX=/data/tbApril2018/data/Run${RUN}
+INPUTDIR_CPX=/data/tbApril2018/clicpix2/Run${RUN}
 
 ${CORRY}/bin/corry -c telescope.conf \
+                   -o histogramFile = "histograms-${1}.root" \
                    -o detectors_file=${CONDFILE} \
 		   -o histogramFile=${HISTOGRAMFILE} \
                    -o Timepix3EventLoader.inputDirectory=${INPUTDIR_TPX} \
-                   -o Clicpix2EventLoader.inputDirectory=${INPUTDIR_CPX}
+                   -o Clicpix2EventLoader.inputDirectory=${INPUTDIR_CPX} \
+                   ###-o OnlineMonitor.canvasTitle="\"CLICdp Online Monitor - Run ${1}\""
+
 
