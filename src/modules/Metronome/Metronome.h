f6d69b6592c49032782c002a0508acc4a95a8c33
Simon Spannagel <simon.spannagel@cern.ch>
2018-06-20 11:09:39 +0200

    Add new module: Metronome

---
+++ src/modules/Metronome/Metronome.h
@@ -0,0 +1,31 @@
+#ifndef Metronome_H
+#define Metronome_H 1
+
+#include <iostream>
+#include "TCanvas.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "core/module/Module.hpp"
+#include "objects/Cluster.h"
+#include "objects/Pixel.h"
+#include "objects/Track.h"
+
+namespace corryvreckan {
+    /** @ingroup Modules
+     */
+    class Metronome : public Module {
+
+    public:
+        // Constructors and destructors
+        Metronome(Configuration config, std::vector<Detector*> detectors);
+        ~Metronome() {}
+
+        // Functions
+        void initialise();
+        StatusCode run(Clipboard* clipboard);
+
+    private:
+        double m_eventStart, m_eventEnd, m_eventLength;
+    };
+} // namespace corryvreckan
+#endif // Metronome_H
---
+++ src/modules/Metronome/README.md
@@ -0,0 +1,17 @@
+## Metronome
+**Maintainer**: Simon Spannagel (<simon.spannagel@cern.ch>)  
+**Status**: Functional  
+
+#### Description
+The `Metronome` module is can be used to slice data without strict event structure in arbitrarily long time slots, which serve as events for subsequent modules. This is done by configuring an event length and by setting the variables `eventStart` and `eventStop` on the clipboard.
+
+Subsequent modules should read these values and adhere to them.
+
+#### Parameters
+* `eventLength`: Length of the event to be defined in physical units (not clock cycles of a specific device). Default value is `10us`.
+
+#### Usage
+```toml
+[Metronome]
+eventLength = 500ns
+```
