dc0b14bdc50979aa3dcf1666309108a037290e9d
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2019-02-22 19:29:35 +0100

    added new functionality: add_trigger_id and has_trigger_id to corryvreckan::event class

--- src/objects/Event.hpp
+++ src/objects/Event.hpp
@@ -17,6 +17,11 @@ namespace corryvreckan {
         double end() const { return end_; };
         double duration() const { return (end_ - timestamp()); };
 
+        void add_trigger_id(uint64_t trigger) { trigger_ids_.push_back(trigger); }
+        bool has_trigger_id(uint64_t trigger) {
+            return std::find(trigger_ids_.begin(), trigger_ids_.end(), trigger) != trigger_ids_.end();
+        }
+
         std::vector<uint64_t> triggers() { return trigger_ids_; };
 
     protected:
