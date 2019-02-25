c8ace86be7783889f1dcf1b50f1b609c771a62ab
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2019-02-25 10:52:09 +0100

    changed Event.hpp: now we have a vector of std::pair{trigger_id, trigger_ts} such that later we can not only check for an existing trigger ID but also retrieve the corresponding time stamp, also added the corresponding function get_trigger_time()

--- src/objects/Event.hpp
+++ src/objects/Event.hpp
@@ -10,23 +10,43 @@ namespace corryvreckan {
     public:
         // Constructors and destructors
         Event(){};
-        Event(double start, double end, std::vector<uint64_t> tigger_ids = std::vector<uint64_t>())
-            : Object(start), end_(end), trigger_ids_(tigger_ids){};
+        Event(double start,
+              double end,
+              std::vector<std::pair<uint32_t, uint64_t>> trigger_list = std::vector<std::pair<uint32_t, uint64_t>>())
+            : Object(start), end_(end), trigger_list_(trigger_list){};
 
         double start() const { return timestamp(); };
         double end() const { return end_; };
         double duration() const { return (end_ - timestamp()); };
 
-        void add_trigger_id(uint64_t trigger) { trigger_ids_.push_back(trigger); }
-        bool has_trigger_id(uint64_t trigger) {
-            return std::find(trigger_ids_.begin(), trigger_ids_.end(), trigger) != trigger_ids_.end();
+        void add_trigger(uint32_t trigger_id, uint64_t trigger_ts) {
+            std::pair<uint32_t, uint64_t> trigger = {trigger_id, trigger_ts};
+            trigger_list_.push_back(trigger);
         }
 
-        std::vector<uint64_t> triggers() { return trigger_ids_; };
+        bool has_trigger_id(uint64_t trigger_id) {
+            for(auto& trigger : trigger_list_) {
+                if(trigger_id == trigger.first) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        uint64_t get_trigger_time(uint64_t trigger_id) {
+            for(auto& trigger : trigger_list_) {
+                if(trigger_id == trigger.first) {
+                    return trigger.second;
+                }
+            }
+            return 0;
+        }
+
+        std::vector<std::pair<uint32_t, uint64_t>> trigger_list() { return trigger_list_; }
 
     protected:
         double end_;
-        std::vector<uint64_t> trigger_ids_{};
+        std::vector<std::pair<uint32_t, uint64_t>> trigger_list_{};
 
         // ROOT I/O class definition - update version number when you change this class!
         ClassDef(Event, 1)
