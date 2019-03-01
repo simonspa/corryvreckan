d3be12b2632abafce46775f8514665c2a042f5fb
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2019-03-01 15:18:22 +0100

    Event.hpp: changed std::vector for trigger list to std::map and simplified the functions add_trigger() and has_trigger_id(), verified functionality by re-analysing run 190 -> I get the same number of TLU/Mimosa events in frame

--- src/objects/Event.hpp
+++ src/objects/Event.hpp
@@ -10,40 +10,24 @@ namespace corryvreckan {
     public:
         // Constructors and destructors
         Event(){};
-        Event(double start,
-              double end,
-              std::vector<std::pair<uint32_t, double>> trigger_list = std::vector<std::pair<uint32_t, double>>())
+        Event(double start, double end, std::map<uint32_t, double> trigger_list = std::map<uint32_t, double>())
             : Object(start), end_(end), trigger_list_(trigger_list){};
 
         double start() const { return timestamp(); };
         double end() const { return end_; };
         double duration() const { return (end_ - timestamp()); };
 
-        void add_trigger(uint32_t trigger_id, double trigger_ts) { trigger_list_.emplace_back(trigger_id, trigger_ts); }
+        void add_trigger(uint32_t trigger_id, double trigger_ts) { trigger_list_.emplace(trigger_id, trigger_ts); }
 
-        bool has_trigger_id(uint64_t trigger_id) const {
-            for(auto& trigger : trigger_list_) {
-                if(trigger_id == trigger.first) {
-                    return true;
-                }
-            }
-            return false;
-        }
+        bool has_trigger_id(uint32_t trigger_id) const { return (trigger_list_.find(trigger_id) != trigger_list_.end()); }
 
-        double get_trigger_time(uint32_t trigger_id) const {
-            for(auto& trigger : trigger_list_) {
-                if(trigger_id == trigger.first) {
-                    return trigger.second;
-                }
-            }
-            return 0;
-        }
+        double get_trigger_time(uint32_t trigger_id) const { return trigger_list_.find(trigger_id)->second; }
 
-        std::vector<std::pair<uint32_t, double>> trigger_list() { return trigger_list_; }
+        std::map<uint32_t, double> trigger_list() { return trigger_list_; }
 
     protected:
         double end_;
-        std::vector<std::pair<uint32_t, double>> trigger_list_{};
+        std::map<uint32_t, double> trigger_list_{};
 
         // ROOT I/O class definition - update version number when you change this class!
         ClassDef(Event, 2)
