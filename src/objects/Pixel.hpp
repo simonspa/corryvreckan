5c42d8c776a03b2f20ae0b6f7e5c49bb5cf237d7
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2019-05-07 14:24:23 +0200

    Pixel.hpp: restructured constructors for easier handling of binary pixels (without timestamp) + incremented ClassDefOverride, also added function bool isBinary()

--- src/objects/Pixel.hpp
+++ src/objects/Pixel.hpp
@@ -13,21 +13,34 @@ namespace corryvreckan {
     public:
         // Constructors and destructors
         Pixel() = default;
-        Pixel(std::string detectorID, int row, int col, int raw) : Pixel(detectorID, row, col, raw, 0.) {}
+
+        // constructors for binary pixels (no charge equivalent value)
+        Pixel(std::string detectorID, int row, int col) : Pixel(detectorID, row, col, 0.) { m_isBinary = true; }
+        Pixel(std::string detectorID, int row, int col, double timestamp) : Pixel(detectorID, row, col, 0, timestamp) {
+            m_isBinary = true;
+        }
+
+        // constructors for pixels with tot or other charge equivalent value
+        Pixel(std::string detectorID, int row, int col, int raw) : Pixel(detectorID, row, col, raw, 0.) {
+            m_isBinary = false;
+        }
         Pixel(std::string detectorID, int row, int col, int raw, double timestamp)
-            : Pixel(detectorID, row, col, raw, timestamp, false) {}
-        Pixel(std::string detectorID, int row, int col, int raw, double timestamp, bool binary)
-            : Object(detectorID, timestamp), m_row(row), m_column(col), m_raw(raw), m_charge(raw), m_isBinary(binary) {}
+            : Object(detectorID, timestamp), m_row(row), m_column(col), m_raw(raw), m_charge(raw) {
+            // FIXME: I don't like that m_charge(raw) is used here...
+            m_isBinary = false;
+        }
 
         int row() const { return m_row; }
         int column() const { return m_column; }
         std::pair<int, int> coordinates() { return std::make_pair(m_column, m_row); }
 
-        // raw is a generic pixel value which can be ToT, ADC, ..., depending on the detector
-        // if isBinary==true, the value will always be 1
-        int raw() const { return (m_isBinary == true ? 1 : m_raw); }
+        // raw is a generic charge equivalent pixel value which can be ToT, ADC, ..., depending on the detector
+        // if isBinary==true, the value will always be 0 and shouldn't be used for anything
+        int raw() const { return m_raw; }
 
         double charge() const { return m_charge; }
+        bool isBinary() const { return m_isBinary; }
+
         void setCharge(double charge) { m_charge = charge; }
         void setRaw(int raw) { m_raw = raw; }
         void setBinary(bool binary) { m_isBinary = binary; }
@@ -41,7 +54,7 @@ namespace corryvreckan {
         /**
          * @brief ROOT class definition
          */
-        ClassDefOverride(Pixel, 6);
+        ClassDefOverride(Pixel, 7);
 
     private:
         // Member variables
