156c127dfd7a84b9331d4a37ea428c02f7c90aae
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2019-05-08 17:13:56 +0200

    Pixel object: clean-up and removed all unnecessary and complicated member variables, also there's only 1 constructor left now

--- src/objects/Pixel.hpp
+++ src/objects/Pixel.hpp
@@ -12,40 +12,36 @@ namespace corryvreckan {
 
     public:
         // Constructors and destructors
+        /**
+         * @brief Required default constructor
+         */
         Pixel() = default;
 
-        // constructors for binary pixels (no charge equivalent value)
-        Pixel(std::string detectorID, int col, int row) : Pixel(detectorID, col, row, 0.) { m_isBinary = true; }
-        Pixel(std::string detectorID, int col, int row, double timestamp) : Pixel(detectorID, col, row, 0, timestamp) {
-            m_isBinary = true;
-        }
-        // I couldn't figure out how to do it like:
-        // Pixel(std::string detectorID, int col, int row, double timestamp) : Pixel(detectorID, col, row, 0, timestamp),
-        // m_isBinary(true) {}
+        /**
+         * @brief Class constructor
+         * @param detectorID detectorID
+         * @param col Pixel column
+         * @param row Pixel row
+         * @param raw Charge-equivalent pixel raw value, initialised to 0
+         * @param charge Pixel charge, initialised to 0
+         * @param timestamp Pixel timestamp, initialised to 0
+         */
+        Pixel(std::string detectorID, int col, int row, int raw, double charge = 0., double timestamp = 0.)
+            : Object(detectorID, timestamp), m_column(col), m_row(row), m_raw(raw), m_charge(charge) {}
 
-        // constructors for pixels with ToT or other charge equivalent value
-        Pixel(std::string detectorID, int col, int row, int raw) : Pixel(detectorID, col, row, raw, 0.) {}
-        Pixel(std::string detectorID, int col, int row, int raw, double timestamp)
-            : Object(detectorID, timestamp), m_column(col), m_row(row), m_raw(raw), m_charge(raw), m_isBinary(false),
-              m_isCalibrated(false) {}
-        // NOTE: m_charge is also initialised with raw, i.e. the uncalibrated value of the charge equivalent measurement
-
+        // Methods to get member variables:
         int row() const { return m_row; }
         int column() const { return m_column; }
-        std::pair<int, int> coordinates() { return std::make_pair(m_column, m_row); }
+        std::pair<int, int> coordinates() const { return std::make_pair(m_column, m_row); }
 
         // raw is a generic charge equivalent pixel value which can be ToT, ADC, ..., depending on the detector
         // if isBinary==true, the value will always be 1 and shouldn't be used for anything
         int raw() const { return m_raw; }
-
         double charge() const { return m_charge; }
-        bool isBinary() const { return m_isBinary; }
-        bool isCalibrated() const { return m_isCalibrated; }
 
-        void setCharge(double charge) { m_charge = charge; }
+        // Methods to set member variables:
         void setRaw(int raw) { m_raw = raw; }
-        // void setBinary(bool binary) { m_isBinary = binary; } // cannot be changed later!
-        void setCalibrated(bool calibrated) { m_isCalibrated = calibrated; }
+        void setCharge(double charge) { m_charge = charge; }
 
         /**
          * @brief Print an ASCII representation of Pixel to the given stream
