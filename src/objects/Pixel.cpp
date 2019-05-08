6b3a22dded24fb565fefe31767ad18df71c17bd7
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2019-05-08 17:16:43 +0200

    Pixel: adjusted print function according to recent changes

--- src/objects/Pixel.cpp
+++ src/objects/Pixel.cpp
@@ -3,6 +3,6 @@
 using namespace corryvreckan;
 
 void Pixel::print(std::ostream& out) const {
-    out << "Pixel " << this->column() << ", " << this->row() << ", " << this->raw() << ", " << this->timestamp()
-        << ", is Binary" << (m_isBinary ? "true" : "false") << ", is calibrated" << (m_isCalibrated ? "true" : "false");
+    out << "Pixel " << this->column() << ", " << this->row() << ", " << this->raw() << ", " << this->charge() << ", "
+        << this->timestamp()
 }
