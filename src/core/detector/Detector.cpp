76183b50a3332f3442dbdd20d95cdf3e373ee661
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2019-05-28 17:51:50 +0200

    Detector: updated member functions getRow(), getColumn(), inPixel()

--- src/core/detector/Detector.cpp
+++ src/core/detector/Detector.cpp
@@ -312,12 +312,19 @@ bool Detector::hitMasked(Track* track, int tolerance) const {
 // Functions to get row and column from local position
 double Detector::getRow(const PositionVector3D<Cartesian3D<double>> localPosition) const {
     // (1-m_nPixelsY%2)/2. --> add 1/2 pixel pitch if even number of rows
-    double row = localPosition.Y() / m_pitch.Y() + static_cast<double>(m_nPixels.Y()) / 2. + (1 - m_nPixels.Y() % 2) / 2.;
+    // double row = localPosition.Y() / m_pitch.Y() + static_cast<double>(m_nPixels.Y()) / 2. + (1 - m_nPixels.Y() % 2) / 2.;
+
+    // I think it's much simpler now: just invert getLocalPosition:
+    double row = localPosition.Y() / m_pitch.Y() + static_cast<double>(m_nPixels.Y() - 1) / 2.;
     return row;
 }
 double Detector::getColumn(const PositionVector3D<Cartesian3D<double>> localPosition) const {
     // (1-m_nPixelsX%2)/2. --> add 1/2 pixel pitch if even number of columns
-    double column = localPosition.X() / m_pitch.X() + static_cast<double>(m_nPixels.X()) / 2. + (1 - m_nPixels.X() % 2) / 2.;
+    // double column = localPosition.X() / m_pitch.X() + static_cast<double>(m_nPixels.X()) / 2. + (1 - m_nPixels.X() % 2)
+    // / 2.;
+
+    // I think it's much simpler now: just invert getLocalPosition:
+    double column = localPosition.X() / m_pitch.X() + static_cast<double>(m_nPixels.X() - 1) / 2.;
     return column;
 }
 
@@ -325,14 +332,18 @@ double Detector::getColumn(const PositionVector3D<Cartesian3D<double>> localPosi
 PositionVector3D<Cartesian3D<double>> Detector::getLocalPosition(double column, double row) const {
 
     return PositionVector3D<Cartesian3D<double>>(
-        m_pitch.X() * (column - m_nPixels.X() / 2.), m_pitch.Y() * (row - m_nPixels.Y() / 2.), 0.);
+        // nPixels-1 to get the right range (for both even and odd numbers)
+        m_pitch.X() * (column - (m_nPixels.X() - 1) / 2.),
+        m_pitch.Y() * (row - (m_nPixels.Y() - 1) / 2.),
+        0.);
 }
 
 // Function to get in-pixel position
 ROOT::Math::XYVector Detector::inPixel(const PositionVector3D<Cartesian3D<double>> localPosition) const {
     double column = getColumn(localPosition);
     double row = getRow(localPosition);
-    return XYVector(m_pitch.X() * (column - floor(column)), m_pitch.Y() * (row - floor(row)));
+    // a pixel ranges from (col-0.5) to (col+0.5)
+    return XYVector(m_pitch.X() * (column - floor(column) - 0.5), m_pitch.Y() * (row - floor(row) - 0.5));
 }
 
 // Check if track position is within ROI:
