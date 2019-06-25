b62b3838f2d53c8eec3c8692218948c65aeeed87
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2019-06-25 15:55:30 +0200

    EventLoaderEUDAQ2: corrected histName for hPixelTriggerTimeResidual_* with underscore, also added if(iTrigger==0) such that the heavy 2D histogram is only created for the 0th trigger to prevent corry from exiting with 'Killed.'

--- src/modules/EventLoaderEUDAQ2/EventLoaderEUDAQ2.cpp
+++ src/modules/EventLoaderEUDAQ2/EventLoaderEUDAQ2.cpp
@@ -346,29 +346,30 @@ StatusCode EventLoaderEUDAQ2::run(std::shared_ptr<Clipboard> clipboard) {
         for(auto& trigger : event->triggerList()) {
             // check if histogram exists already, if not: create it
             if(hPixelTriggerTimeResidual.find(iTrigger) == hPixelTriggerTimeResidual.end()) {
-                std::string histName = "hPixelTriggerTimeResidual" + to_string(iTrigger);
+                std::string histName = "hPixelTriggerTimeResidual_" + to_string(iTrigger);
                 std::string histTitle = histName + ";trigger_ts - pixel_ts [us];# entries";
                 hPixelTriggerTimeResidual[iTrigger] = new TH1D(histName.c_str(), histTitle.c_str(), 2e5, -100, 100);
             }
             // I know, the histogram below should always exist when the one above exists but I think it's bad
             // practice to put it into the above if statement as well...
             if(hPixelTriggerTimeResidualOverTime.find(iTrigger) == hPixelTriggerTimeResidualOverTime.end()) {
-                if(iTrigger != 0) {
-                    // If I don't continue here, corry ends with message "Killed"
+                if(iTrigger == 0) {
+                    // If I don't use this if here, corry ends with message "Killed"
                     // Do I get too many memory-hungy 2D plots again here?
-                    continue;
+                    std::string histName = "hPixelTriggerTimeResidualOverTime_" + to_string(iTrigger);
+                    std::string histTitle = histName + ";time [us];trigger_ts - pixel_ts [us];# entries";
+                    hPixelTriggerTimeResidualOverTime[iTrigger] =
+                        new TH2D(histName.c_str(), histTitle.c_str(), 3e3, 0, 3e3, 1e4, -50, 50);
                 }
-                std::string histName = "hPixelTriggerTimeResidualOverTime_" + to_string(iTrigger);
-                std::string histTitle = histName + ";time [us];trigger_ts - pixel_ts [us];# entries";
-                hPixelTriggerTimeResidualOverTime[iTrigger] =
-                    new TH2D(histName.c_str(), histTitle.c_str(), 3e3, 0, 3e3, 1e4, -50, 50);
             }
             // use iTrigger, not trigger ID (=trigger.first) (which is unique and continuously incrementing over the runtime)
             hPixelTriggerTimeResidual[iTrigger]->Fill(
                 static_cast<double>(Units::convert(pixel->timestamp() - trigger.second, "us")));
-            hPixelTriggerTimeResidualOverTime[iTrigger]->Fill(
-                static_cast<double>(Units::convert(pixel->timestamp(), "s")),
-                static_cast<double>(Units::convert(pixel->timestamp() - trigger.second, "us")));
+            if(iTrigger == 0) {
+                hPixelTriggerTimeResidualOverTime[iTrigger]->Fill(
+                    static_cast<double>(Units::convert(pixel->timestamp(), "s")),
+                    static_cast<double>(Units::convert(pixel->timestamp() - trigger.second, "us")));
+            }
             iTrigger++;
         }
     }
