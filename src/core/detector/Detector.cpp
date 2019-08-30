e396c64045b136841c81d4cea7d98b861379149b
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2019-08-30 15:42:42 +0200

    Detector: cleanup of comments/removed personal remarks

--- src/core/detector/Detector.cpp
+++ src/core/detector/Detector.cpp
@@ -325,19 +325,12 @@ bool Detector::hitMasked(Track* track, int tolerance) const {
 
 // Functions to get row and column from local position
 double Detector::getRow(const PositionVector3D<Cartesian3D<double>> localPosition) const {
-    // (1-m_nPixelsY%2)/2. --> add 1/2 pixel pitch if even number of rows
-    // double row = localPosition.Y() / m_pitch.Y() + static_cast<double>(m_nPixels.Y()) / 2. + (1 - m_nPixels.Y() % 2) / 2.;
-
-    // I think it's much simpler now: just invert getLocalPosition:
+    // just invert getLocalPosition:
     double row = localPosition.Y() / m_pitch.Y() + static_cast<double>(m_nPixels.Y() - 1) / 2.;
     return row;
 }
 double Detector::getColumn(const PositionVector3D<Cartesian3D<double>> localPosition) const {
-    // (1-m_nPixelsX%2)/2. --> add 1/2 pixel pitch if even number of columns
-    // double column = localPosition.X() / m_pitch.X() + static_cast<double>(m_nPixels.X()) / 2. + (1 - m_nPixels.X() % 2)
-    // / 2.;
-
-    // I think it's much simpler now: just invert getLocalPosition:
+    // just invert getLocalPosition:
     double column = localPosition.X() / m_pitch.X() + static_cast<double>(m_nPixels.X() - 1) / 2.;
     return column;
 }
