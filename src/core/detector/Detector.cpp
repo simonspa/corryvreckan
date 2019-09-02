71ed6a04da1e69ac3b01a13c72d8b79c69e75cbe
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2019-09-02 13:57:34 +0200

    Detector.cpp: putting old conversion for getLocalPosition, getColumn and getRow in place

--- src/core/detector/Detector.cpp
+++ src/core/detector/Detector.cpp
@@ -325,13 +325,11 @@ bool Detector::hitMasked(Track* track, int tolerance) const {
 
 // Functions to get row and column from local position
 double Detector::getRow(const PositionVector3D<Cartesian3D<double>> localPosition) const {
-    // just invert getLocalPosition:
-    double row = localPosition.Y() / m_pitch.Y() + static_cast<double>(m_nPixels.Y() - 1) / 2.;
+    double row = localPosition.Y() / m_pitch.Y() + static_cast<double>(m_nPixels.Y()) / 2. + (1 - m_nPixels.Y() % 2) / 2.;
     return row;
 }
 double Detector::getColumn(const PositionVector3D<Cartesian3D<double>> localPosition) const {
-    // just invert getLocalPosition:
-    double column = localPosition.X() / m_pitch.X() + static_cast<double>(m_nPixels.X() - 1) / 2.;
+    double column = localPosition.X() / m_pitch.X() + static_cast<double>(m_nPixels.X()) / 2. + (1 - m_nPixels.X() % 2) / 2.;
     return column;
 }
 
@@ -339,10 +337,7 @@ double Detector::getColumn(const PositionVector3D<Cartesian3D<double>> localPosi
 PositionVector3D<Cartesian3D<double>> Detector::getLocalPosition(double column, double row) const {
 
     return PositionVector3D<Cartesian3D<double>>(
-        // nPixels-1 to get the right range (for both even and odd numbers)
-        m_pitch.X() * (column - (m_nPixels.X() - 1) / 2.),
-        m_pitch.Y() * (row - (m_nPixels.Y() - 1) / 2.),
-        0.);
+        m_pitch.X() * (column - (m_nPixels.X()) / 2.), m_pitch.Y() * (row - (m_nPixels.Y()) / 2.), 0.);
 }
 
 // Function to get in-pixel position
