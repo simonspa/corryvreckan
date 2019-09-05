ece76d4797dcfa785d62fce5a2c5621e41c1da2c
Simon Spannagel <simon.spannagel@cern.ch>
2019-09-05 10:07:11 +0200

    Objects: solve performance issue by not cleaning up TRef dependants.

--- src/objects/Object.cpp
+++ src/objects/Object.cpp
@@ -7,7 +7,12 @@ Object::Object(std::string detectorID) : m_detectorID(std::move(detectorID)) {}
 Object::Object(double timestamp) : m_timestamp(timestamp) {}
 Object::Object(std::string detectorID, double timestamp) : m_detectorID(std::move(detectorID)), m_timestamp(timestamp) {}
 Object::Object(const Object&) = default;
-Object::~Object() = default;
+Object::~Object() {
+    // In Corryvreckan, all objects are destroyed together, i.e. at the end of the event. To avoid costly reverse-iterations
+    // through the TRef dependency hash lists, we just tell ROOT not to care about possible TRef-dependants and to just
+    // destroy the object directly by resetting the `kMustCleanup` bit.
+    this->ResetBit(kMustCleanup);
+}
 
 std::ostream& corryvreckan::operator<<(std::ostream& out, const Object& obj) {
     obj.print(out);
