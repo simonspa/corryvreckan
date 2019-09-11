e649cec1a632271c3093695ea03b7961eda67e94
Morag Williams <morag.williams@cern.ch>
2019-09-11 14:58:40 +0200

    Removing detectors with role  from AlignmentTrackChi2 module

--- src/modules/AlignmentTrackChi2/AlignmentTrackChi2.cpp
+++ src/modules/AlignmentTrackChi2/AlignmentTrackChi2.cpp
@@ -159,7 +159,7 @@ void AlignmentTrackChi2::finalise() {
             string detectorID = detector->name();
 
             // Do not align the reference plane
-            if(detector->isReference() || detector->isDUT()) {
+            if(detector->isReference() || detector->isDUT() || detector->isAuxiliary()) {
                 continue;
             }
 
@@ -253,7 +253,7 @@ void AlignmentTrackChi2::finalise() {
     // Now list the new alignment parameters
     for(auto& detector : get_detectors()) {
         // Do not align the reference plane
-        if(detector->isReference() || detector->isDUT()) {
+        if(detector->isReference() || detector->isDUT() || detector->isAuxiliary()) {
             continue;
         }
 
