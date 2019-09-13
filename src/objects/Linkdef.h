9f455f5ef459d8a1d0f13155310ef0391ef06729
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2019-09-13 10:49:43 +0200

    re-ran formatting after swithing pre-commit hook to clang-format-6.0

--- src/objects/Linkdef.h
+++ src/objects/Linkdef.h
@@ -8,10 +8,9 @@
 #pragma link off all functions;
 
 // Missing ROOT objects
-#pragma link C++ class ROOT::Math::Cartesian2D < unsigned int> + ;
-#pragma link C++ class ROOT::Math::DisplacementVector2D < ROOT::Math::Cartesian2D < unsigned int>,                          \
-    ROOT::Math::DefaultCoordinateSystemTag> +                                                                               \
-    ;
+#pragma link C++ class ROOT::Math::Cartesian2D < unsigned int > +;
+#pragma link C++ class ROOT::Math::DisplacementVector2D < ROOT::Math::Cartesian2D < unsigned int >,                         \
+    ROOT::Math::DefaultCoordinateSystemTag > +;
 
 // Corryvreckan objects
 #pragma link C++ class corryvreckan::Object + ;
@@ -25,4 +24,4 @@
 #pragma link C++ class corryvreckan::Event + ;
 
 // Vector of Object for internal storage
-#pragma link C++ class std::vector < corryvreckan::Object*> + ;
+#pragma link C++ class std::vector < corryvreckan::Object* > +;
