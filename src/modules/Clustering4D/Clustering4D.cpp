8d17d920ee2f8ff1f39c28e9bd4dcdb73a6999c5
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2019-11-06 18:39:04 +0100

    Clustering4D: renaming 'resolution' -> 'spatial_resolution'

--- src/modules/Clustering4D/Clustering4D.cpp
+++ src/modules/Clustering4D/Clustering4D.cpp
@@ -229,8 +229,8 @@ void Clustering4D::calculateClusterCentre(Cluster* cluster) {
     cluster->setRow(row);
     cluster->setCharge(charge);
 
-    // Set uncertainty on position from intrinstic detector resolution:
-    cluster->setError(m_detector->resolution());
+    // Set uncertainty on position from intrinstic detector spatial resolution:
+    cluster->setError(m_detector->spatial_resolution());
 
     cluster->setTimestamp(timestamp);
     cluster->setDetectorID(detectorID);
