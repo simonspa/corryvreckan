a8c8e611e7a06489e70741a405780d26989486a7
Simon Spannagel <simon.spannagel@cern.ch>
2019-12-16 16:24:04 +0100

    CMake: warn about outdated/missing git hook

--- cmake/clang-cpp-checks.cmake
+++ cmake/clang-cpp-checks.cmake
@@ -1,5 +1,19 @@
 # Additional targets to perform clang-format/clang-tidy/cppcheck
 
+# Check if the git pre-commit hook for formatting is installed:
+IF(IS_DIRECTORY ${CMAKE_SOURCE_DIR}/.git)
+    SET(HOOK_SRC "${CMAKE_SOURCE_DIR}/etc/git-hooks/pre-commit-clang-format-hook")
+    SET(HOOK_DST "${CMAKE_SOURCE_DIR}/.git/hooks/pre-commit-clang-format")
+    IF(NOT EXISTS ${HOOK_DST})
+        MESSAGE(WARNING "Git hooks are not installed - consider installing them via ${CMAKE_SOURCE_DIR}/etc/git-hooks/install-hooks.sh")
+    ELSE()
+        EXECUTE_PROCESS(COMMAND "cmake" "-E" "compare_files" ${HOOK_SRC} ${HOOK_DST} RESULT_VARIABLE HOOKS_DIFFER)
+        IF(${HOOKS_DIFFER})
+            MESSAGE(WARNING "Git hooks are outdated - consider updating them via ${CMAKE_SOURCE_DIR}/etc/git-hooks/install-hooks.sh")
+        ENDIF()
+    ENDIF()
+ENDIF()
+
 # Get all project files - FIXME: this should also use the list of generated targets
 IF(NOT CHECK_CXX_SOURCE_FILES)
     MESSAGE(FATAL_ERROR "Variable CHECK_CXX_SOURCE_FILES not defined - set it to the list of files to auto-format")
