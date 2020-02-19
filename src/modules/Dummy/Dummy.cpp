e661b796db878be4e3f0b1bb8601d1267b9cf89a
Simon Spannagel <simon.spannagel@cern.ch>
2020-02-19 22:09:59 +0100

    Mode license note sin module headers, adapted dummy module

--- src/modules/Dummy/Dummy.cpp
+++ src/modules/Dummy/Dummy.cpp
@@ -1,7 +1,8 @@
 /**
  * @file
- * @brief Implementation of [Dummy] module
- * Copyright (c) 2019 CERN and the Corryvreckan authors.
+ * @brief Implementation of module Dummy
+ *
+ * @copyright Copyright (c) 2020 CERN and the Corryvreckan authors.
  * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
  * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
  * Intergovernmental Organization or submit itself to any jurisdiction.
--- src/modules/EtaCalculation/EtaCalculation.h
+++ src/modules/EtaCalculation/EtaCalculation.h
@@ -1,3 +1,13 @@
+/**
+ * @file
+ * @brief Definition of module EtaCalculation
+ *
+ * @copyright Copyright (c) 2017-2020 CERN and the Corryvreckan authors.
+ * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
+ * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
 #ifndef EtaCalculation_H
 #define EtaCalculation_H 1
 
