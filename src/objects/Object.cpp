d3386af07d57b2a65e252d472556d74e315389e0
Lennart Huth <lennart.huth@desy.de>
2020-03-11 17:54:00 +0100

    next attempt

--- src/objects/Object.cpp
+++ src/objects/Object.cpp
@@ -17,7 +17,6 @@ Object::Object(std::string detectorID) : m_detectorID(std::move(detectorID)) {}
 Object::Object(double timestamp) : m_timestamp(timestamp) {}
 Object::Object(std::string detectorID, double timestamp) : m_detectorID(std::move(detectorID)), m_timestamp(timestamp) {}
 Object::Object(const Object&) = default;
-Object::~Object() = default;
 
 std::ostream& corryvreckan::operator<<(std::ostream& out, const Object& obj) {
     obj.print(out);
