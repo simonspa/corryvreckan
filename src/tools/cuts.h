e6acbf457e2aa766a923df788a16b8a33cce2d29
Simon Spannagel <simon.spannagel@cern.ch>
2020-05-04 11:19:15 +0200

    Make cuts tool work for single detectors and lists thereof

--- src/tools/cuts.h
+++ src/tools/cuts.h
@@ -37,11 +37,11 @@ namespace corryvreckan {
     }
 
     template <typename T>
-    static std::map<std::shared_ptr<Detector>, T> calculate_cut(const std::string& name,
-                                                                double rel_default,
-                                                                const Configuration& config,
-                                                                const std::vector<std::shared_ptr<Detector>>& detectors) {
-        std::map<std::shared_ptr<Detector>, T> cut_map;
+    static T calculate_cut(const std::string& name,
+                           double rel_default,
+                           const Configuration& config,
+                           const std::shared_ptr<Detector>& detector) {
+
         std::string absolute = name + "_abs";
         std::string relative = name + "_rel";
 
@@ -49,19 +49,25 @@ namespace corryvreckan {
             throw InvalidCombinationError(
                 config, {relative, absolute}, "Absolute and relative cuts are mutually exclusive.");
         } else if(config.has(absolute)) {
-            for(auto& detector : detectors) {
-                cut_map[detector] = config.get<T>(absolute);
-            }
+            return config.get<T>(absolute);
         } else {
-            for(auto& detector : detectors) {
-                try {
-                    cut_map[detector] = get_resolution<T>(name, detector) * config.get<double>(relative, rel_default);
-                } catch(ConfigurationError&) {
-                    throw InvalidValueError(config, relative, "key doesn't match requested resolution type");
-                }
+            try {
+                return get_resolution<T>(name, detector) * config.get<double>(relative, rel_default);
+            } catch(ConfigurationError&) {
+                throw InvalidValueError(config, relative, "key doesn't match requested resolution type");
             }
         }
+    }
 
+    template <typename T>
+    static std::map<std::shared_ptr<Detector>, T> calculate_cut(const std::string& name,
+                                                                double rel_default,
+                                                                const Configuration& config,
+                                                                const std::vector<std::shared_ptr<Detector>>& detectors) {
+        std::map<std::shared_ptr<Detector>, T> cut_map;
+        for(const auto& detector : detectors) {
+            cut_map[detector] = calculate_cut<T>(name, rel_default, config, detector);
+        }
         return cut_map;
     }
 
