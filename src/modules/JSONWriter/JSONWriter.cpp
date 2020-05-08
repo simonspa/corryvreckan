3f0797870603874bb6c05852666f769b005756e4
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2020-05-08 10:29:42 +0200

    JSONWriter: write default cuts to config before reading -> will be written out with config instead of blank

--- src/modules/JSONWriter/JSONWriter.cpp
+++ src/modules/JSONWriter/JSONWriter.cpp
@@ -23,8 +23,9 @@ void JSONWriter::initialise() {
     }
 
     // Create output file
+    config_.setDefault<std::string>("file_name", "data");
     output_file_name_ =
-        createOutputFile(corryvreckan::add_file_extension(config_.get<std::string>("file_name", "data"), "json"), true);
+        createOutputFile(corryvreckan::add_file_extension(config_.get<std::string>("file_name"), "json"), true);
     output_file_ = std::make_unique<std::ofstream>(output_file_name_);
 
     *output_file_ << "[" << std::endl;
