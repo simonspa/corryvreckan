27e5991abb88e234a3530ce240fbdbc592abbe63
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2020-05-08 10:31:16 +0200

    Textwriter: write default cuts to config before reading -> will be written out with config instead of blank

--- src/modules/TextWriter/TextWriter.cpp
+++ src/modules/TextWriter/TextWriter.cpp
@@ -23,8 +23,9 @@ void TextWriter::initialise() {
     }
 
     // Create output file
+    config_.setDefault<std::string>("file_name", "data");
     output_file_name_ =
-        createOutputFile(corryvreckan::add_file_extension(config_.get<std::string>("file_name", "data"), "txt"), true);
+        createOutputFile(corryvreckan::add_file_extension(config_.get<std::string>("file_name"), "txt"), true);
     output_file_ = std::make_unique<std::ofstream>(output_file_name_);
 
     *output_file_ << "# Corryvreckan ASCII data" << std::endl << std::endl;
