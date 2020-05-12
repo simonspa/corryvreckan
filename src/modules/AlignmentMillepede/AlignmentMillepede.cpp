e97ca7d4abf4a447b19f95d82163ff18e0825331
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2020-05-12 16:48:49 +0200

    Revert "Try to find out why AppleClang behaves the way it does"
    This reverts commit 7463aca13b5e46ee51c8cb614422a7433d492d98.

--- src/modules/AlignmentMillepede/AlignmentMillepede.cpp
+++ src/modules/AlignmentMillepede/AlignmentMillepede.cpp
@@ -26,8 +26,6 @@ AlignmentMillepede::AlignmentMillepede(Configuration& config, std::vector<std::s
     : Module(config, std::move(detectors)) {
 
     config_.setDefault<bool>("exclude_dut", false);
-    // config_.setDefaultArray<bool>("dofs", {});
-    // config_.setDefaultArray<bool>("dofs", {true, true, false, true, true, true});
     config_.setDefault<size_t>("iterations", 5);
     config_.setDefault<double>("residual_cut", 0.05);
     config_.setDefault<double>("residual_cut_init", 0.6);
