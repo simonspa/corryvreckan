3768c3543e69eb88d61acddf53000c030566901b
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2020-06-18 17:20:54 +0200

    EventLoaderTimepix3: rename histogra
    (cherry picked from commit b77d1fff8c582cfcaf3daf01c3920b963c969a09)

--- src/modules/EventLoaderTimepix3/EventLoaderTimepix3.cpp
+++ src/modules/EventLoaderTimepix3/EventLoaderTimepix3.cpp
@@ -156,7 +156,7 @@ void EventLoaderTimepix3::initialize() {
     m_file_iterator = m_files.begin();
 
     // Calibration
-    pixelToT_beforecalibration = new TH1F("pixelToT_beforecalibration", "pixelToT_beforecalibration", 100, -0.5, 199.5);
+    pixelToT_beforecalibration = new TH1F("pixelToT", "pixelToT", 100, -0.5, 199.5);
 
     if(m_detector->isDUT() && config_.has("calibration_path") && config_.has("threshold")) {
         LOG(INFO) << "Applying calibration from " << calibrationPath;
