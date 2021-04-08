6d6de5de4e350ca88994c32e7e4d504997a34de6
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2021-04-08 16:41:39 +0200

    ImproveReferenceTimestamp: cleanup (remove methods + add new histograms and parameter search window)

--- src/modules/ImproveReferenceTimestamp/ImproveReferenceTimestamp.cpp
+++ src/modules/ImproveReferenceTimestamp/ImproveReferenceTimestamp.cpp
@@ -17,18 +17,28 @@ using namespace std;
 ImproveReferenceTimestamp::ImproveReferenceTimestamp(Configuration& config, std::vector<std::shared_ptr<Detector>> detectors)
     : Module(config, std::move(detectors)) {
 
-    config_.setDefault<int>("improvement_method", 1);
     config_.setDefault<std::string>("signal_source", "W0013_G02");
     config_.setDefault<double>("trigger_latency", Units::get<double>(0, "ns"));
+    config_.setDefault<double>("search_window", Units::get<double>(100, "ns"));
 
-    m_method = config_.get<int>("improvement_method");
     m_source = config_.get<std::string>("signal_source");
     m_triggerLatency = config_.get<double>("trigger_latency");
+    m_searchWindow = config_.get<double>("search_window");
 }
 
 void ImproveReferenceTimestamp::initialize() {
     // Initialise member variables
     m_eventNumber = 0;
+    m_improvedTriggers = 0;
+    m_noTriggerFound = 0;
+
+    hTriggersPerEvent = new TH1D("hTriggersPerEvent", "hTriggersPerEvent;triggers per event;# entries", 20, -0.5, 19.5);
+    hTracksPerEvent = new TH1D("hTracksPerEvent", "hTracksPerEvent;tracks per event;# entries", 20, -0.5, 19.5);
+    hTrackTriggerTimeCorrelation = new TH1D("hTrackTriggerTimeCorrelation",
+                                            "hTrackTriggerTimeCorrelation;ts_{track} - ts_{trigger} [ns];# entries",
+                                            1000,
+                                            -500.5,
+                                            499.5);
 }
 
 StatusCode ImproveReferenceTimestamp::run(const std::shared_ptr<Clipboard>& clipboard) {
@@ -49,15 +59,25 @@ StatusCode ImproveReferenceTimestamp::run(const std::shared_ptr<Clipboard>& clip
     // Get the tracks from the clipboard
     auto tracks = clipboard->getData<Track>();
     LOG(DEBUG) << "Number of tracks found: " << tracks.size();
+
+    hTriggersPerEvent->Fill(static_cast<double>(trigger_times.size()));
+    hTracksPerEvent->Fill(static_cast<double>(tracks.size()));
+
     for(auto& track : tracks) {
 
         double improved_time = track->timestamp();
 
-        // Use trigger timestamp
-        if(m_method == 0) {
-            // Find trigger timestamp closest in time
-            double diff = std::numeric_limits<double>::max();
+        // Find trigger timestamp closest in time
+        double diff = std::numeric_limits<double>::max();
+        if(!trigger_times.size()) {
+            // set to non-sense timestamp because we cannot delete from clipboard
+            track->setTimestamp(-1);
+            m_noTriggerFound++;
+        } else {
             for(auto& trigger_time : trigger_times) {
+
+                hTrackTriggerTimeCorrelation->Fill(track->timestamp() - trigger_time);
+
                 LOG(DEBUG) << " track: " << track->timestamp() << " trigger: " << trigger_time
                            << " diff: " << Units::display(abs(trigger_time - track->timestamp()), {"ns", "us", "s"})
                            << " diff stored: " << Units::display(diff, {"ns", "us", "s"});
@@ -66,26 +86,14 @@ StatusCode ImproveReferenceTimestamp::run(const std::shared_ptr<Clipboard>& clip
                     diff = abs(trigger_time - track->timestamp());
                 }
             }
-        }
-
-        // Use average track timestamp
-        else if(m_method == 1) {
-            int nhits = 0;
-            double avg_track_time = 0;
-            for(auto& cluster : track->getClusters()) {
-                avg_track_time += cluster->timestamp();
-                nhits++;
+            if(diff < m_searchWindow) {
+                track->setTimestamp(improved_time);
+                m_improvedTriggers++;
+            } else {
+                track->setTimestamp(-1);
+                m_noTriggerFound++;
             }
-            improved_time = round(avg_track_time / nhits);
-            LOG(DEBUG) << setprecision(12) << "Reference track time "
-                       << Units::display(track->timestamp(), {"ns", "us", "s"});
-            LOG(DEBUG) << setprecision(12) << "Average track time " << Units::display(improved_time, {"ns", "us", "s"});
         }
-
-        // Set improved reference timestamp
-        track->setTimestamp(improved_time);
-
-        LOG(DEBUG) << "End of track";
     }
 
     // Increment event counter
@@ -98,5 +106,6 @@ StatusCode ImproveReferenceTimestamp::run(const std::shared_ptr<Clipboard>& clip
 
 void ImproveReferenceTimestamp::finalize(const std::shared_ptr<ReadonlyClipboard>&) {
 
-    LOG(DEBUG) << "Analysed " << m_eventNumber << " events";
+    LOG(STATUS) << "Analysed " << m_eventNumber << " events, improved track timestamps: " << m_improvedTriggers
+                << ", no trigger found: " << m_noTriggerFound;
 }
