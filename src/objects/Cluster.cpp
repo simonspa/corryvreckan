a6b4162d89eb41171ff3067302aec4dd9f7ba9dc
Jens Kroeger <kroeger@physi.uni-heidelberg.de>
2021-06-07 16:36:52 +0200

    Cluster: cleaner to use numeric_limits::lowest

--- src/objects/Cluster.cpp
+++ src/objects/Cluster.cpp
@@ -51,7 +51,7 @@ std::vector<const Pixel*> Cluster::pixels() const {
 const Pixel* Cluster::getSeedPixel() const {
     Pixel* seed = nullptr;
 
-    double maxcharge = -std::numeric_limits<double>::max();
+    double maxcharge = std::numeric_limits<double>::lowest();
     // loop overall pixels and find the one with the largest charge:
     for(auto& px : m_pixels) {
         auto pxl = dynamic_cast<Pixel*>(px.GetObject());
