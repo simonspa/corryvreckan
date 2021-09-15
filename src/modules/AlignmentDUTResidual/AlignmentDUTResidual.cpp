9b08aaecc4313bc66c594abcd66bb0dd3cd92a2d
Lennart Huth <lennart.huth@desy.de>
2021-09-15 11:02:36 +0200

    Typo in comment

--- src/modules/AlignmentDUTResidual/AlignmentDUTResidual.cpp
+++ src/modules/AlignmentDUTResidual/AlignmentDUTResidual.cpp
@@ -85,7 +85,7 @@ StatusCode AlignmentDUTResidual::run(const std::shared_ptr<Clipboard>& clipboard
     // Make a local copy and store it
     for(auto& track : tracks) {
         auto associated_clusters = track->getAssociatedClusters(m_detector->getName());
-        // Do not put tracks without clusters on the DUT to the persistant storage
+        // Do not put tracks without clusters on the DUT to the persistent storage
         if(associated_clusters.size() == 0) {
             LOG(TRACE) << "Discarding track for DUT alignment since no cluster associated";
             continue;
