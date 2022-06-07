045098c4b63b389668b07251e486a58b1c603164
Lennart Huth <lennart.huth@cern.ch>
2022-06-07 10:02:13 +0200

    fixing doc

--- src/modules/EventLoaderMuPixTelescope/EventLoaderMuPixTelescope.h
+++ src/modules/EventLoaderMuPixTelescope/EventLoaderMuPixTelescope.h
@@ -32,7 +32,7 @@ namespace corryvreckan {
         /**
          * @brief Constructor for this unique module
          * @param config Configuration object for this module as retrieved from the steering file
-         * @param detector Shared pointer to detector of this module
+         * @param detectors Shared pointer to detectors of this module, detectors are assigned via name/tag pairs
          */
         EventLoaderMuPixTelescope(Configuration& config, std::vector<std::shared_ptr<Detector>> detectors);
         ~EventLoaderMuPixTelescope() {}
